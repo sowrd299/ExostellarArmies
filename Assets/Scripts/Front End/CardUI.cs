cf773ab042347d0cb7e56f8fc8c971bc2dda2f06
dmelkony@uci.edu <dmelkony@uci.edu>
2019-01-30 00:21:49 -0800

    little more UI

---
+++ Assets/Scripts/Front End/CardUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardUI : MonoBehaviour
+{
+    public Text name;
+    public Text detail;
+    public Text flavor;
+    public Text type;
+
+    public CardFrontEnd card;
+
+    private void Start()
+    {
+        LoadCard(card);
+    }
+
+    public void LoadCard(CardFrontEnd c)
+    {
+        card = c;
+        name.text = c.cardName;
+        detail.text = c.cardDetail;
+        flavor.text = c.cardFlavor;
+        type.text = c.cardType;
+    }
+}
