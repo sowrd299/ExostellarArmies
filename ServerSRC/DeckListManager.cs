635961ca9ad7ffe35fad1d046d04ea90c32cd59b
sowrd299 <ryan.ward46@gmail.com>
2019-03-07 18:56:26 -0800

    Add some abilities to hardcoded cards

--- ServerSRC/DeckListManager.cs
+++ ServerSRC/DeckListManager.cs
@@ -23,7 +23,7 @@ namespace SFB.Net.Server{
                     "Binding the Stars", 0,1,2, new AbilityList()),1);
 
             r.AddCard(new UnitCard(5, "Adv. Infantry Support System", Faction.CARTH, "Melee Shield 1\nPersistent: At the start of your turn, generate an extra resource.",
-                    "Mobile Command Center", 0, 3, 6, new AbilityList()),2);
+                    "Mobile Command Center", 0, 3, 6, new AbilityList(new Ability[]{new MeleeShield(1)})),2);
 
             r.AddCard(new UnitCard(4, "Paladin-Class XS Marnines", Faction.CARTH, "Front Line Recurring Deploy: If there is not a unit behind this, heal each adjacent allied front line 1.",
                     "Defense Against the Darkness", 1, 3, 5, new AbilityList()),1);
@@ -35,13 +35,13 @@ namespace SFB.Net.Server{
                     "Tuning Blades in the Darkness", 1, 2, 4, new AbilityList()), 1);
 
             r.AddCard(new UnitCard(4, "Mgr. Lt. Tul Yorves", Faction.CARTH, "Unique; Ranged Shield 1\nFront Line Persistent: Whenever you heal a Unit, heal it for 1 more and it deals 1 damage.",
-                    "Master Medic", 1, 3, 5, new AbilityList()),1);
+                    "Master Medic", 1, 3, 5, new AbilityList(new Ability[]{new RangedShield(2)})),1);
 
             r.AddCard(new UnitCard(3, "Exostellar Snipers", Faction.CARTH, "",
                     "Illuminate with Muzzle Flare!", 3,1,3, new AbilityList()),2);
 
             r.AddCard(new UnitCard(4, "Cmdr. Yos Lorth", Faction.CARTH, "Unique; Ranged Shield 1\nFront Line Persistent: Allied front line Elites have +1 and Ranged Shield +1",
-                    "Exostellar Champion", 2, 2, 5, new AbilityList()),1);
+                    "Exostellar Champion", 2, 2, 5, new AbilityList(new Ability[]{new RangedShield(1)})),1);
 
             r.AddCard(new UnitCard(2, "Ancillary Medical Officer", Faction.CARTH, "Supporting Carthan Infantry Deploy: Heal this front line 2.",
                     "Healer in the Darkness", 1, 2, 3, new AbilityList()),1);
@@ -53,7 +53,7 @@ namespace SFB.Net.Server{
                     "756328", 0, 0, 1, new AbilityList()),1);
             
             r.AddCard(new UnitCard(2, "Cannoneer Drone", Faction.CARTH, "Ranged Shield 2; Melee Shield 2\nFront Line Persistent: The tower behind this deals +3 damge.",
-                    "Death to Your Enemies", 0, 0, 1, new AbilityList()),1);
+                    "Death to Your Enemies", 0, 0, 1, new AbilityList(new Ability[]{new RangedShield(2), new MeleeShield(2)})),1);
 
             r.AddCard(new UnitCard(2, "Autonomous Range Finder", Faction.CARTH, "Supporting Carthan Deploy: Give this front line +3R this turn.",
                     "56413", 0, 1, 3),2);
@@ -68,20 +68,23 @@ namespace SFB.Net.Server{
             deck.AddCard(new UnitCard(2, "Commercial Shipper",Faction.NONE, "Deploy: Draw a card, then put a card from your hand on the bottom of your deck", "Whatever you need, we got", 0,3,3, new AbilityList()), 1);
             deck.AddCard(new UnitCard(2, "Adaptive Protopod", Faction.MYXOR, "Support Infantry: This front line gains Spore 3 until end of turn.\nSupport Artillery: This gains Siege 1 until end of turn", 
                "MyxI jha maRtox lOz!", 2, 0, 2, new AbilityList()), 2);
-            deck.AddCard(new UnitCard(1, "Screaming Spore Spunner", Faction.MYXOR, "Spore 1(when this dies, gain 1 resource)", "Muxi ji maRtox raj!", 0,2,2, new AbilityList()), 1);
+            deck.AddCard(new UnitCard(1, "Screaming Spore Spunner", Faction.MYXOR, "Spore 1 (when this dies, gain 1 resource)", "Muxi ji maRtox raj!",
+            		0,2,2, new AbilityList(new Ability[]{new Spore(1)})), 1);
             deck.AddCard(new UnitCard(1, "Pod-Cap Yxuu maRroz", Faction.MYXOR, "Unique\nDeploy:Allied Infantry get +1M this turn.", "Far-Flung Mind-Spore Pod",1,0,2, new AbilityList()), 1);
             deck.AddCard(new UnitCard(5, "Undergrowth Smasher", Faction.MYXOR, "Siege 1\nDeploy: Deal 1 damage to the enemy in front of this for each Allied Myxori Unit",
-               "Myxi jha maRtox lOz!", 0, 4, 4, new AbilityList()), 1);
-            deck.AddCard(new UnitCard(2, "Aftergrowth Spunner", Faction.MYXOR, "Spore  3(when this dies, gain 3 resources)", "toxhMar IOz Yzhmat rho!", 0, 2, 2, new AbilityList()), 1);
+               "Myxi jha maRtox lOz!", 0, 4, 4, new AbilityList(new Ability[]{new Siege(1)})), 1);
+            deck.AddCard(new UnitCard(2, "Aftergrowth Spunner", Faction.MYXOR, "Spore  3 (when this dies, gain 3 resources)", "toxhMar IOz Yzhmat rho!",
+					0, 2, 2, new AbilityList(new Ability[]{new Siege(3)})), 1);
             deck.AddCard(new UnitCard(2, "Mulch Pod", Faction.MYXOR, "Absorb\nDeath:Beta Swarm (if there is nothing in this back row, reveal the top card of your deck and play it at -1 Cost. If you can't, put it in your graveyard)",
-               "Myxi jha maRtox IOz", 0, 1, 1, new AbilityList()), 2);
+               "Myxi jha maRtox IOz", 0, 1, 1, new AbilityList(new Ability[]{new Absorb()})), 2);
             deck.AddCard(new UnitCard(2, "Fecund Reflex Axon", Faction.MYXOR, "Deploy...\nSupporting Myxori: Gain 3 Resources and this gets +3R this turn.\n" +
                "Front Line:If there is no back line here, this gains Absorb until the end of turn.", "Myxi jha maRtox IOz", 0, 2, 2, new AbilityList()), 2);
             deck.AddCard(new UnitCard(1, "Grafted Fang Spunner", Faction.MYXOR, "Death:Beta Swarm (if there is nothing in this back row, reveal the top card of your deck and play it at -1 Cost. If you can't, put it in your graveyard)",
                "Myxi jha maRtox IOz", 0,2,2, new AbilityList()), 2);
-            deck.AddCard(new UnitCard(1, "Spore Pod", Faction.MYXOR, "Spore 1(when this dies, gain 1 resource)\nDeath:Beta Swarm (if there is nothing in this back row, reveal the top card of your deck and play it at -1 Cost. If you can't, put it in your graveyard)",
-               "Myxi jha maRtox IOz", 0, 0, 1, new AbilityList()), 2);
-            deck.AddCard(new UnitCard(3, "Horde Caller", Faction.MYXOR, "Spore 2(when this dies, gain 3 resources)\nDeath: +1 Extra deploy", "MuxhRaz izt zraZxh",0,3,2, new AbilityList()), 2);
+            deck.AddCard(new UnitCard(1, "Spore Pod", Faction.MYXOR, "Spore 1 (when this dies, gain 1 resource)\nDeath:Beta Swarm (if there is nothing in this back row, reveal the top card of your deck and play it at -1 Cost. If you can't, put it in your graveyard)",
+               "Myxi jha maRtox IOz", 0, 0, 1, new AbilityList(new Ability[]{new Spore(1)})), 2);
+            deck.AddCard(new UnitCard(3, "Horde Caller", Faction.MYXOR, "Spore 2 (when this dies, gain 3 resources)\nDeath: +1 Extra deploy", "MuxhRaz izt zraZxh",
+					0,3,2, new AbilityList(new Ability[]{new Spore(2)})), 2);
             deck.AddCard(new UnitCard(6, "Titanous Regrowth Trunk", Faction.MYXOR, "Persistent: Other allies gain Spore +1(when this dies, gain 1 resource)\n" +
                "Front Line Persistent: Whenever an adjacent Font Line Infantry dies, Beta Swarm there", "Myxi jha maRtox lOz!", 0, 3, 7, new AbilityList()), 2);
 
