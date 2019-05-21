4af4ce86b43e9fb264027144e3df0d9228d9e4d5
syy1125 <syy1125@hotmail.com>
2019-05-20 20:47:02 -0700

    Implement dynamic loading in DeckListManager

--- ServerSRC/DeckListManager.cs
+++ ServerSRC/DeckListManager.cs
@@ -1,101 +1,84 @@
-using SFB.Game.Content;
-using SFB.Game;
-using System.Collections.Generic;
 using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using YamlDotNet.RepresentationModel;
+using SFB.Game;
+using SFB.Game.Content;
 
 // this is part of Server, and not Game.Deck, because it (in theory) needs contact to
 // databases, account information, etc.
 // also the client shouldn't have access to it
 // at minimum it is not part of the pure abstract gamelogic
-namespace SFB.Net.Server{
-    
-    class DeckListManager{
-        CardLoader newCardLoader = new CardLoader();
+namespace SFB.Net.Server
+{
 
+	class DeckListManager
+	{
+		private CardLoader cardLoader;
+		private Dictionary<string, DeckList> decks;
 
-        // hardcode carthan starter deck
-        private DeckList CarthStarter(){
-            DeckList r = new DeckList();
-            r.AddCard(newCardLoader.GetByID("Exostellar Marine Squad"), 2);
-            
-            r.AddCard(newCardLoader.GetByID("Commercial Shipper"), 1); // no system
+		public DeckListManager()
+		{
+			cardLoader = new CardLoader();
 
-            r.AddCard(newCardLoader.GetByID("Commercial Coms Relay"),1); // no system
+			decks = new Dictionary<string, DeckList>();
+			LoadDecks();
+		}
 
-            r.AddCard(newCardLoader.GetByID("Adv. Infantry Support System"),2);
+		private void LoadDecks()
+		{
+			string path = Path.Combine("Assets", "Resources", "Decks");
+			foreach (string fileName in Directory.EnumerateFiles(path, "*.yaml"))
+			{
+				YamlStream yaml = new YamlStream();
+				yaml.Load(File.OpenText(fileName));
 
-            r.AddCard(newCardLoader.GetByID("Paladin-Class XS Marines"),1);
+				YamlMappingNode root = yaml.Documents[0].RootNode as YamlMappingNode;
+				string deckId = root.Children[new YamlScalarNode("id")].ToString();
 
-            r.AddCard(newCardLoader.GetByID("Adv. Supply Drone"),2);
-
-            r.AddCard(newCardLoader.GetByID("XS Field Technician"), 1);
-
-            r.AddCard(newCardLoader.GetByID("Lt. Mgr. Tul Yorves"),1); // no system yet
-
-            r.AddCard(newCardLoader.GetByID("Exostellar Snipers"),2);
-
-            r.AddCard(newCardLoader.GetByID("Cmdr. Yos Lorth"),1);
-
-            r.AddCard(newCardLoader.GetByID("Ancillary Medical Officer"),1);
-
-            r.AddCard(newCardLoader.GetByID("Battle-Line Trauma Medic"),1); // not yet
-            
-            r.AddCard(newCardLoader.GetByID("Emergency Med Drop"),1);
-            
-            r.AddCard(newCardLoader.GetByID("Cannoneer Drone"),1); // not yet
-
-            r.AddCard(newCardLoader.GetByID("Autonomous Range Finder"), 2);
-            return r;
-        }
-
-        // hardcoded Myxorit starter deck
-        private DeckList MyxoriStarter()
-        {
-            DeckList deck = new DeckList();
-
-            deck.AddCard(newCardLoader.GetByID("Mercenary Phantasm"), 1); // no system
-            deck.AddCard(newCardLoader.GetByID("Commercial Shipper"), 1); // no system
-            deck.AddCard(newCardLoader.GetByID("Adaptive Protopod"), 2);
-            deck.AddCard(newCardLoader.GetByID("Screaming Spore Spunner"), 1);
-            deck.AddCard(newCardLoader.GetByID("Pod-Cap Yxuu maRroz"), 1);
-            deck.AddCard(newCardLoader.GetByID("Undergrowth Smasher"), 1);
-            deck.AddCard(newCardLoader.GetByID("Aftergrowth Spunner"), 1);
-            deck.AddCard(newCardLoader.GetByID("Mulch Pod"), 2);
-            deck.AddCard(newCardLoader.GetByID("Fecund Reflex Axon"), 2);
-            deck.AddCard(newCardLoader.GetByID("Grafted Fang Spunner"), 2);
-            deck.AddCard(newCardLoader.GetByID("Spore Pod"), 2);
-            deck.AddCard(newCardLoader.GetByID("Horde Caller"), 2);
-            deck.AddCard(newCardLoader.GetByID("Titanous Regrowth Trunk"), 2);
-
-            return deck;
-        }
-
-        // return a decklist object of the deck list with the
-        // given ID
-        // TODO: implement
-        public DeckList LoadFromID(string id){
-            //TESTING IMPLEMENTATION
-            Console.WriteLine("Loading deck: {0}...", id);
-			if(id.StartsWith("TEST ")) {
-				DeckList d = new DeckList();
-				d.AddCard(newCardLoader.GetByID(id.Substring(5)), 8);
-				d.AddCard(newCardLoader.GetByID("Resist Token"), 6);
-				d.AddCard(newCardLoader.GetByID("Mana Token"), 6);
-				Console.WriteLine("ok");
-				return d;
-			} else {
-				switch(id) {
-					case "carthStarter":
-						return CarthStarter();
-					case "myxoriStarter":
-						return MyxoriStarter();
-					case "testing":
-					default:
-						DeckList d = new DeckList();
-						d.AddCard(newCardLoader.GetByID("Resist Token"), 20);
-						return d;
+				DeckList deck = new DeckList();
+				YamlSequenceNode cardSequence = root.Children[new YamlScalarNode("cards")] as YamlSequenceNode;
+				foreach (YamlMappingNode cardItem in cardSequence)
+				{
+					deck.AddCard(
+						cardLoader.GetByID(cardItem.Children[new YamlScalarNode("id")].ToString()),
+						int.Parse(cardItem.Children[new YamlScalarNode("count")].ToString())
+					);
 				}
+
+				decks.Add(deckId, deck);
+				Console.WriteLine($"Loaded deck {deckId} ({deck.GetCards().Select(deck.GetCopiesOf).Sum()} cards).");
 			}
-        }
-    }
+		}
+
+		// return a decklist object of the deck list with the
+		// given ID
+		public DeckList LoadFromID(string id)
+		{
+			Console.WriteLine($"Loading deck: {id}");
+			if (decks.ContainsKey(id))
+			{
+				return decks[id];
+			}
+
+			//TESTING IMPLEMENTATION
+			if (id.StartsWith("TEST "))
+			{
+				DeckList d = new DeckList();
+				d.AddCard(cardLoader.GetByID(id.Substring(5)), 8);
+				d.AddCard(cardLoader.GetByID("Resist Token"), 6);
+				d.AddCard(cardLoader.GetByID("Mana Token"), 6);
+				Console.WriteLine("Loaded test deck");
+				return d;
+			}
+			else
+			{
+				// TODO: What's the default deck?
+				DeckList d = new DeckList();
+				d.AddCard(cardLoader.GetByID("Resist Token"), 20);
+				return d;
+			}
+		}
+	}
 }
