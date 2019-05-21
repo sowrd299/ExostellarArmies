4469bb7c3d506f7155a4aec0bc9114dfd97fa8f5
syy1125 <syy1125@hotmail.com>
2019-05-21 16:38:55 -0700

    Improve hover card overlay
    Hover card now shows colorized stats.
    The stats color is based on how the unit stats compares to the card stats.

--- Assets/Scripts/Front End/HoverCardOverlay.cs
+++ Assets/Scripts/Front End/HoverCardOverlay.cs
@@ -20,6 +20,8 @@ public class HoverCardOverlay : MonoBehaviour, IPointerEnterHandler, IPointerExi
 	private Graphic[] cardGraphics => _cardGraphics ?? (_cardGraphics = GetComponentsInChildren<Graphic>());
 	private Card _card;
 	private Card card => _card ?? (_card = GetComponent<IHasCard>().card);
+	private UnitUI _unitUI;
+	private UnitUI unitUI => _unitUI ?? (_unitUI = GetComponent<UnitUI>());
 
 	public void OnPointerEnter(PointerEventData eventData)
 	{
@@ -27,7 +29,52 @@ public class HoverCardOverlay : MonoBehaviour, IPointerEnterHandler, IPointerExi
 
 		CardUI hoverCardUI = hoverObject.GetComponent<CardUI>();
 		hoverCardUI.cardData = card;
+		if (unitUI != null)
+		{
+			// Detect that this is a unit by the presence of unit ui.
+			// If so, apply override.
+			ApplyUnitOverrides(hoverCardUI, unitUI.unit);
+		}
 
+		ZoomHover();
+
+		CanvasGroup hoverGroup = hoverObject.GetComponent<CanvasGroup>();
+		hoverGroup.alpha = 0;
+		UIManager.instance.LerpTime(
+			Mathf.Lerp,
+			0f, 1f,
+			fadeDuration, alpha => hoverGroup.alpha = alpha
+		);
+
+		hoverObject.GetComponent<HoverCardOverlay>().enabled = false;
+	}
+
+	private void ApplyUnitOverrides(CardUI cardUI, Unit unit)
+	{
+		cardUI.RenderCard();
+		cardUI.enabled = false;
+
+		ApplyOverrideAspect(unit.HealthPoints, unit.Card.HealthPoints, cardUI.health);
+		ApplyOverrideAspect(unit.RangedAttack, unit.Card.RangedAttack, cardUI.rangedDamage);
+		ApplyOverrideAspect(unit.MeleeAttack, unit.Card.MeleeAttack, cardUI.meleeDamage);
+	}
+
+	private void ApplyOverrideAspect(int unitValue, int cardValue, Text valueText)
+	{
+		if (unitValue < cardValue)
+		{
+			valueText.text = unitValue.ToString();
+			valueText.color = Color.red;
+		}
+		else if (unitValue > cardValue)
+		{
+			valueText.text = unitValue.ToString();
+			valueText.color = Color.green;
+		}
+	}
+
+	private void ZoomHover()
+	{
 		// Max zoom to help players see better. That's kinda the whole point of this hover thing.
 		RectTransform hoverParentTransform = hoverParent.GetComponent<RectTransform>();
 		RectTransform hoverCardTransform = hoverObject.GetComponent<RectTransform>();
@@ -36,14 +83,6 @@ public class HoverCardOverlay : MonoBehaviour, IPointerEnterHandler, IPointerExi
 			hoverParentTransform.rect.width / hoverCardTransform.rect.width,
 			hoverParentTransform.rect.height / hoverCardTransform.rect.height
 		);
-
-		foreach (Graphic cardGraphic in hoverObject.GetComponent<HoverCardOverlay>().cardGraphics)
-		{
-			cardGraphic.CrossFadeAlpha(0, 0, true);
-			cardGraphic.CrossFadeAlpha(1, fadeDuration, true);
-		}
-
-		hoverObject.GetComponent<HoverCardOverlay>().enabled = false;
 	}
 
 	public void OnPointerExit(PointerEventData eventData)
@@ -60,10 +99,12 @@ public class HoverCardOverlay : MonoBehaviour, IPointerEnterHandler, IPointerExi
 	{
 		if (hoverObject != null)
 		{
-			foreach (Graphic cardGraphic in hoverObject.GetComponent<HoverCardOverlay>().cardGraphics)
-			{
-				cardGraphic.CrossFadeAlpha(0, fadeDuration, true);
-			}
+			CanvasGroup hoverGroup = hoverObject.GetComponent<CanvasGroup>();
+			UIManager.instance.LerpTime(
+				Mathf.Lerp,
+				1f, 0f,
+				fadeDuration, alpha => { if (hoverGroup != null) hoverGroup.alpha = alpha; }
+			);
 
 			Destroy(hoverObject, fadeDuration);
 		}
