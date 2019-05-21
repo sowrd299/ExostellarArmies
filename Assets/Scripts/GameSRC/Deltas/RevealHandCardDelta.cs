a4d9198e99981826ebbedad46307887ae3e82cc0
Joseph Michels <bobelement4181@gmail.com>
2019-05-20 18:22:32 -0700

    Reveal Card in Hand

---
+++ Assets/Scripts/GameSRC/Deltas/RevealHandCardDelta.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml;
+using System;
+using SFB.Game.Management;
+using SFB.Game.Content;
+
+namespace SFB.Game
+{
+	public abstract class RevealHandCardDelta : Delta
+	{
+        private int Index;
+
+        public RevealHandCardDelta(CardList list, int i)
+        {
+			Index = i;
+        }
+
+        public RevealHandCardDelta(XmlElement from, CardLoader cl)
+        {
+            this.Index = Int32.Parse(from.Attributes["index"].Value);
+        }
+
+        public override XmlElement ToXml(XmlDocument doc)
+		{
+            XmlElement r = base.ToXml(doc);
+
+            XmlAttribute indexAttr = doc.CreateAttribute("index");
+            indexAttr.Value = Index.ToString();
+            r.SetAttributeNode(indexAttr);
+
+            return r;
+        }
+        
+        internal override void Apply() {}
+        internal override void Revert() {}
+    }
+}
