2250d189c671e15bd033533c42bd5e00581bc0b2
Joseph Michels <bobelement4181@gmail.com>
2019-06-03 18:31:24 -0700

    unit ability maintext

---
+++ Assets/Scripts/GameSRC/Abilities/CommercialComsRelay.cs
@@ -0,0 +1,32 @@
+using SFB.Game.Management;
+using System.Collections.Generic;
+
+namespace SFB.Game
+{
+	public class CommercialShipper : Ability
+	{
+		public override string GetMainText() {
+			return "Deploy: Draw a card, then put a card from hand on the bottom of your deck.";
+		}
+
+		public CommercialShipper() : base(-1) { }
+
+		protected override void AddEffectsToEvents(Unit u, GameManager gm)
+		{
+			u.AddInitialDeployDeltas += InnerCommercialShipper;
+		}
+
+		protected override void RemoveEffectsFromEvents(Unit u, GameManager gm)
+		{
+			u.AddInitialDeployDeltas -= InnerCommercialShipper;
+		}
+
+		public void InnerCommercialShipper(List<Delta> delta, GMWithLocation gmLoc)
+		{
+			System.Console.WriteLine("COMM SHIP INIT DEP");
+			CommercialShipperIR inputRequest = new CommercialShipperIR(
+				gmLoc.SubjectPlayer, gmLoc.GameManager
+			);
+		}
+	}
+}
